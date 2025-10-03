aa4dc119adc61cf1948e614256177dfe0813f66a
Vladislav <77727894+vladgorchakov@users.noreply.github.com>
2025-10-03 09:30:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from time import sleep
-#import requests
 import socket
 import network
 from machine import Pin
+from config import Config as conf
 
 
 # Инициализация микроконтроллера и самоблокировка питания
@@ -10,7 +10,7 @@ from machine import Pin
 
 # Включение питания маршрутизатора
 # Подключение к маршрутизатору 
-class WifiRouter:
+class WiFiRouter:
     def __init__(self, pin: int):
         self.switch = Pin(pin, Pin.OUT)
 
@@ -33,52 +33,54 @@ class WifiRouter:
 # Проверка доступа в Интернет (сделать пинг, проверить статус)
 # При условии, что активен маршрутизатор
 class ConnectionChecker:
-    def __init__(self, ip="8.8.8.8", port=53):
+    def __init__(self, wlan, ip="8.8.8.8", port=53):
         self.ip = ip #Можно список IP для проверки в виде кортежа ((ip:port), (ip:port))
         self.port = port
+        self.__wlan = wlan
         self.__external_is_connect = False
         self.__internal_is_connect = False
 
-    def check_internal_network(self, wlan):
-        for i in range(15): 
-            if wlan.isconnected():
-                self.__external_is_connect = True
-                return True
-            print('.', end='')
+    def __check_internal_network(self):
+        self.__internal_is_connect = False
+        for i in range(15):
+            if self.__wlan.isconnected():
+                self.__internal_is_connect = True
+                break
             sleep(1)
-        
-        self.__external_is_connect = False
-        print("error: internal wifi connection does not exist")
-        
-        return False
 
-    
-    def check_external_network(self):
+
+    def __check_external_network(self):
         # Установить таумаут для ожидания ответа от сервера
         # Можно больше, так как канал связи через GSM
-        client = socket.socket()
-        try:
-            client.connect((self.ip, self.port))
-        except Exception as e:
+        if not self.__internal_is_connect:
             self.__external_is_connect = False
-            print(f"error: external wifi connection does not exist\n details: {e}")
         else:
-            self.__external_is_connect = True
+            client = socket.socket()
+            try:
+                client.connect((self.ip, self.port))
+            except:
+                self.__external_is_connect = False
+            else:
+                self.__external_is_connect = True
+
+    @property
+    def internal_is_connect(self):
+        self.__check_internal_network()
+
+        return self.__internal_is_connect
+
+    @property
+    def external_is_connect(self):
+        self.__check_internal_network()
+        self.__check_external_network()
 
         return self.__external_is_connect
-    
-    def check(self, wlan):
-        if self.check_internal_network(wlan):
-            if self.check_external_network():
-                return True
-        return False
 
 
-class wifiConnection:
-    def __init__(self, user, password, conn_checker):
+class WiFiConnection:
+    def __init__(self, user, password):
         self.user = user
         self.password = password
-        self.conn_checker = conn_checker
         self.__wlan = None
 
     @property
@@ -89,10 +91,8 @@ class wifiConnection:
         self.__wlan = network.WLAN()
         self.__wlan.active(True)
         if not self.__wlan.isconnected():
-            print('connecting to network:', end=' ')
             self.__wlan.connect(self.user, self.password)
-        self.conn_checker.check_internal_network(self.__wlan)
-        
+
         return self.__wlan
 
         
@@ -107,12 +107,16 @@ class wifiConnection:
 # Опрос кнопок 
 #   Если кнопка активна, то осуществить вывод запрашиваемой информации на OLED дисплей
 
-router = WifiRouter(26)
-conn_status = ConnectionChecker()
-wifi_connection = wifiConnection('b76lop', 'chirp339', conn_status)
+router = WiFiRouter(26)
+wifi_connection = WiFiConnection(conf.wifi_ssid, conf.wifi_password)
+conn_status = ConnectionChecker(wifi_connection)
 
 router.start()
 if router.state:
-    wifi_connection.create_wlan()
-    if conn_status.check(wifi_connection.wlan):
-        print("Соединение установлено!")
+    wlan = wifi_connection.create_wlan()
+    conn_status = ConnectionChecker(wlan)
+    print(conn_status.internal_is_connect)
+    print(conn_status.external_is_connect)
+
+
+
